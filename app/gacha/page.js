ae94472757e1db0d5c4788e758223375e96f09d3
ssiddarthsai1999 <ssiddarthsai1999@gmail.com>
2026-07-25 18:55:38 +0530

    feat: gacha pack-open reveal animation (charge, burst, rarity reveal)

--- app/gacha/page.js
+++ app/gacha/page.js
@@ -1,11 +1,18 @@
+"use client";
+
+import { useState } from "react";
 import HomeHero from "../components/home/HomeHero";
 import FeaturedDrops from "../components/home/FeaturedDrops";
+import PackOpenModal from "../components/home/PackOpenModal";
 
 export default function GachaPage() {
+  const [opening, setOpening] = useState(false);
+
   return (
     <main className="relative min-h-screen">
-      <HomeHero />
+      <HomeHero onOpen={() => setOpening(true)} />
       <FeaturedDrops />
+      <PackOpenModal open={opening} onClose={() => setOpening(false)} />
     </main>
   );
 }
